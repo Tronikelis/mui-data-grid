e942164b399f1e45d5509c86926e5a683a0f4737
Tronikel <56039679+Trunkelis@users.noreply.github.com>
2021-09-28 18:24:41 +0300

    start do add minimize-maximize font

---
+++ src/toolbar/index.ts
@@ -0,0 +1,1 @@
+export { default as MinMaxFont } from "./min-max-font";
---
+++ src/toolbar/min-max-font.tsx
@@ -0,0 +1,7 @@
+import { Box } from "@mui/material";
+import AddIcon from "@mui/icons-material/Add";
+import RemoveIcon from "@mui/icons-material/Remove";
+
+export default function MinMaxFont() {
+    return <div>Test</div>;
+}
