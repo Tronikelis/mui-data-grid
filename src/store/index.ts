8ff87386391472e77d9f8d9a94847270d7e6b063
Tronikel <56039679+Trunkelis@users.noreply.github.com>
2021-09-30 14:36:34 +0300

    style

--- src/store/index.ts
+++ src/store/index.ts
@@ -19,29 +19,31 @@ interface TableStore {
     };
 }
 
-export const useTableStore = create<TableStore>(immer(set => ({
-    state: {
-        rows: [],
-        fontSize: 1,
+export const useTableStore = create<TableStore>(
+    immer(set => ({
+        state: {
+            rows: [],
+            fontSize: 1,
 
-        sortDirection: false,
-        sortBy: "",
-    },
-    actions: {
-        incrementFont: () => set(store => {
-            store.state.fontSize += 0.1;
-        }),
-        decrementFont: () => set(store => {
-            store.state.fontSize -= 0.1;
-        }),
-        setSortDirection: () => set(store => {
-            store.state.sortDirection = !store.state.sortDirection;
-        }),
-        setSortBy: by => set(store => {
-            store.state.sortBy = by;
-        }),
-        setRows: rows => set(store => {
-            store.state.rows = rows;
-        }),
-    },
-})));
+            sortDirection: false,
+            sortBy: "",
+        },
+        actions: {
+            incrementFont: () => set(store => {
+                store.state.fontSize += 0.1;
+            }),
+            decrementFont: () => set(store => {
+                store.state.fontSize -= 0.1;
+            }),
+            setSortDirection: () => set(store => {
+                store.state.sortDirection = !store.state.sortDirection;
+            }),
+            setSortBy: by => set(store => {
+                store.state.sortBy = by;
+            }),
+            setRows: rows => set(store => {
+                store.state.rows = rows;
+            }),
+        },
+    })
+));
